693c8ae89eca24f6184d92456a1c4348cf2d164f
mateors <bill.rassel@gmail.com>
2021-02-07 05:19:31 +0600

    array data type

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//primitive data types
+//rune, byte = alias
+//int, float32,string, bool
+
+//Composite Data Types Maps
+//array
+// bracket = []
+// braces/curly braces = {}
+// parenthesis = ()
+
+//var students [3]string
+//fixed length
+//same type
+//index count start from 0
+
+//0,1,2,3,4 
+//data set or data assign
+//students[0]="Asgor"
+//students[1]="Mainul"
+//students[2]="Anonnya"
+
+//[  ]
+//fmt.Println(students)
+//fmt.Println(len(students))
+
+//data pull, data retrieve, data get
+//fmt.Println(students[2])
+
+//short hand way, string literals
+//In computer science, a literal is a notation 
+//for representing a fixed value in source code
+//implicit= ...
+
+students := [...]string{"Asgor", "Mainul", "Anonnya", "Mostain", "Sumaiya"}
+fmt.Println(students, len(students))
+
+
+
+//slice
+//map
+//struct
+
+ 
+
+}
