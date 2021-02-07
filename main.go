a4004b827f93da784b7a4c13ab2c533562f824ea
mateors <bill.rassel@gmail.com>
2021-02-07 06:26:06 +0600

    slice done

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "reflect"
 
 func main(){
 
@@ -15,9 +16,24 @@ func main(){
 // parenthesis = ()
 
 //slice
-//map
-//struct
+//more than array
 
+//var students [3]string
+//students[0]="Asgor"
+//students[1]="Mainul"
+//students[2]="Anonnya"
+//x := students[0:3]
+
+//x := make([]string, 0)
+var fruits []string
+fruits = append(fruits, "Apple", "Banana", "Mango")
+//fmt.Println(fruits, len(fruits))
+
+//fmt.Printf("%T\n", fruits)
+//fmt.Printf("%T\n", students)
+
+b := reflect.TypeOf(fruits).Kind().String()
+fmt.Println(b)
  
 
 }
